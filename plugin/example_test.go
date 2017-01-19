1c61ca94b76f97156f0854c28c1c08d5b9d5dabd
Vladimir Skipor <skipor@yandex-team.ru>
2017-01-19 16:26:20 +0300

    #27: MLP -> MPL

--- plugin/example_test.go
+++ plugin/example_test.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2017 Yandex LLC. All rights reserved.
-// Use of this source code is governed by a MLP 2.0
+// Use of this source code is governed by a MPL 2.0
 // license that can be found in the LICENSE file.
 // Author: Vladimir Skipor <skipor@yandex-team.ru>
 
