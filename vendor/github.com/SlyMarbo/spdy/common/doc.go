2d23c70744310859ec523b080b143563aedb9a4b
Alexey Lavrenuke <direvius@gmail.com>
2017-05-15 14:49:04 +0300

    fix #43

--- vendor/github.com/SlyMarbo/spdy/common/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2014 Jamie Hall. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package common contains shared functionality for the spdy package.
-//
-package common
--- vendor/github.com/SlyMarbo/spdy/spdy2/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2014 Jamie Hall. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package spdy2 contains functionality for SPDY/2.
-package spdy2
