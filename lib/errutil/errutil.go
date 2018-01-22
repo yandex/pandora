7bc8c92abf422e920de6eb8fcbd66a50611aa884
Vladimir Skipor <skipor@yandex-team.ru>
2018-01-22 13:04:24 +0300

    errutil pkg

---
+++ lib/errutil/errutil.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2018 Yandex LLC. All rights reserved.
+// Use of this source code is governed by a MPL 2.0
+// license that can be found in the LICENSE file.
+// Author: Vladimir Skipor <skipor@yandex-team.ru>
+
+package errutil
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
+)
+
+type StackTracer interface {
+	StackTrace() errors.StackTrace
+}
+
+// FIXME(skipor): test
+func Join(err1, err2 error) error {
+	switch {
+	case err1 == nil:
+		return err2
+	case err2 == nil:
+		return err1
+	default:
+		return multierror.Append(err1, err2)
+	}
+}
+
+func IsNotCtxError(ctx context.Context, err error) bool {
+	if err == nil {
+		return false
+	}
+	select {
+	case <-ctx.Done():
+		if ctx.Err() == errors.Cause(err) { // Support github.com/pkg/errors wrapping
+			return false
+		}
+	default:
+	}
+	return true
+}
---
+++ lib/errutil/errutil_suite_test.go
@@ -0,0 +1,39 @@
+package errutil
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
+
+	"github.com/yandex/pandora/lib/testutil"
+)
+
+func TestErrutil(t *testing.T) {
+	testutil.RunSuite(t, "Errutil Suite")
+}
+
+var _ = Describe("Iscoreutil.IsNotCtxErroror", func() {
+	canceledContext, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	It("nil error", func() {
+		Expect(IsNotCtxError(context.Background(), nil)).To(BeFalse())
+	})
+
+	It("context error", func() {
+		Expect(IsNotCtxError(canceledContext, context.Canceled)).To(BeFalse())
+	})
+
+	It("caused by context error", func() {
+		Expect(IsNotCtxError(canceledContext, errors.Wrap(context.Canceled, "new err"))).To(BeFalse())
+	})
+
+	It("usual error", func() {
+		err := errors.New("new err")
+		Expect(IsNotCtxError(canceledContext, err)).To(BeTrue())
+		Expect(IsNotCtxError(context.Background(), err)).To(BeTrue())
+	})
+})
