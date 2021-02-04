19a2794c851bad214a17b61893e3b29a31506412
ligreen <ligreen@yandex-team.ru>
2021-02-04 17:41:57 +0300

    add step validation and test
    ref:10b664892da638e82ce5aa82013c55ef07217037

--- core/schedule/step.go
+++ core/schedule/step.go
@@ -26,9 +26,9 @@ func NewStep(from, to float64, step int64, duration time.Duration) core.Schedule
 }
 
 type StepConfig struct {
-	From     float64 `validate:"min=0"`
-	To       float64 `validate:"min=0"`
-	Step     int64
+	From     float64       `validate:"min=0"`
+	To       float64       `validate:"min=0"`
+	Step     int64         `validate:"min=1"`
 	Duration time.Duration `validate:"min-time=1ms"`
 }
 
