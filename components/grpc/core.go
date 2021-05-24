cae5667f398306ad46ba9392603d36264e77f360
ligreen <ligreen@yandex-team.ru>
2021-05-24 22:22:41 +0300

    fix for Services, that does not expose the ServiceDescriptor
    ref:96e9fd88edab9ab47240002d9cd1d17691687deb

--- components/grpc/core.go
+++ components/grpc/core.go
@@ -8,10 +8,12 @@ import (
 
 	"github.com/yandex/pandora/core"
 	"github.com/yandex/pandora/core/aggregator/netsample"
+
 	"github.com/jhump/protoreflect/desc"
 	"github.com/jhump/protoreflect/dynamic"
 	"github.com/jhump/protoreflect/dynamic/grpcdynamic"
 	"github.com/jhump/protoreflect/grpcreflect"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 	reflectpb "google.golang.org/grpc/reflection/grpc_reflection_v1alpha"
@@ -61,18 +63,23 @@ func (g *Gun) Bind(aggr core.Aggregator, deps core.GunDeps) error {
 	g.GunDeps = deps
 	g.stub = grpcdynamic.NewStub(conn)
 
+	log := deps.Log
+
 	meta := make(metadata.MD)
 	refCtx := metadata.NewOutgoingContext(context.Background(), meta)
 	refClient := grpcreflect.NewClient(refCtx, reflectpb.NewServerReflectionClient(conn))
 	listServices, err := refClient.ListServices()
 	if err != nil {
-		log.Fatalf("Fatal: failed to get services list\n %s\n", err)
+		log.Fatal("Fatal: failed to get services list\n %s\n", zap.Error(err))
 	}
 	g.services = make(map[string]desc.MethodDescriptor)
 	for _, s := range listServices {
 		service, err := refClient.ResolveService(s)
 		if err != nil {
-			log.Fatalf("FATAL ResolveService: %s", err)
+			if grpcreflect.IsElementNotFoundError(err) {
+				continue
+			}
+			log.Fatal("FATAL ResolveService: %s", zap.Error(err))
 		}
 		listMethods := service.GetMethods()
 		for _, m := range listMethods {
@@ -103,7 +110,6 @@ func (g *Gun) shoot(ammo *Ammo) {
 		return
 	}
 
-	log.Printf("MethodDescriptor: %s", method.GetInputType())
 	payloadJSON, err := json.Marshal(ammo.Payload)
 	if err != nil {
 		log.Fatalf("FATAL: Payload parsing error %s\n", err)
@@ -115,7 +121,7 @@ func (g *Gun) shoot(ammo *Ammo) {
 	err = message.UnmarshalJSON(payloadJSON)
 	if err != nil {
 		code = 400
-		log.Printf("BAD REQUEST: %s", err)
+		log.Printf("BAD REQUEST: %s\n", err)
 		return
 	}
 
