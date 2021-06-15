a325ea34623414219f9d467a96b6d88ea2cf1b9d
ligreen <ligreen@yandex-team.ru>
2021-06-16 00:02:03 +0300

    PR from branch users/ligreen/pandora-uripost
    update uripost test
    
    add decoder test
    
    ref:6588b612c035a64a55370afdd8aacc7b9965b2fe

--- components/phttp/ammo/simple/uripost/decoder.go
+++ components/phttp/ammo/simple/uripost/decoder.go
@@ -15,7 +15,7 @@ type Header struct {
 
 func decodeHeader(line []byte) (key string, val string, err error) {
 	if len(line) < 3 || line[0] != '[' || line[len(line)-1] != ']' {
-		return key, val, errors.New("header line should be like '[key: value]")
+		return key, val, errors.New("header line should be like '[key: value]'")
 	}
 	line = line[1 : len(line)-1]
 	colonIdx := bytes.IndexByte(line, ':')
@@ -33,10 +33,13 @@ func decodeHeader(line []byte) (key string, val string, err error) {
 func decodeURI(uriString []byte) (bodySize int, uri string, tag string, err error) {
 	parts := strings.Split(string(uriString), " ")
 	bodySize, err = strconv.Atoi(parts[0])
+	if err != nil {
+		err = errors.New("Wrong ammo body size, should be in bytes")
+		return
+	}
 	switch {
-	case len(parts) <= 2:
+	case len(parts) == 2:
 		uri = parts[1]
-
 	case len(parts) >= 3:
 		uri = parts[1]
 		tag = parts[2]
