c89292ca47ecb3c70343343059725c9842234f5a
sabevzenko <sabevzenko@yandex-team.com>
2024-06-25 12:35:21 +0300

    discard overflow sliding window
    discard overflow sliding window
    119a0330b912b4023932f3612b25cb36c8792fed

--- cli/cli.go
+++ cli/cli.go
@@ -25,7 +25,7 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-const Version = "0.5.27"
+const Version = "0.5.28"
 const defaultConfigFile = "load"
 const stdinConfigSelector = "-"
 
--- docs/rus/best_practices.md
+++
@@ -1,3 +0,0 @@
-# Практики использования
-
-- [RPS per instance](./best_practices/rps_per_instance.md)
